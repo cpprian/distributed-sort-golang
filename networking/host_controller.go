0e6e357f202e44aaed9eb865acc255f27377093f
cpprian <cpprian456@gmail.com>
2025-06-05 00:46:10 +0200

    move some func to another files

---
+++ networking/host_controller.go
@@ -0,0 +1,75 @@
+package networking
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/libp2p/go-libp2p/core/peer"
+	host "github.com/libp2p/go-libp2p/core/host"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+func (l *Libp2pHost) GetListenAddress() string {
+	return l.listenAddr.String()
+}
+
+func (l *Libp2pHost) Start(ctx context.Context) error {
+	if err := l.host.Network().Listen(l.listenAddr); err != nil {
+		return fmt.Errorf("failed to start host: %w", err)
+	}
+	log.Println("Host started successfully")
+	return nil
+}
+
+func (l *Libp2pHost) Stop() error {
+	if err := l.host.Close(); err != nil {
+		return fmt.Errorf("failed to stop host: %w", err)
+	}
+	log.Println("Host stopped successfully")
+	return nil
+}
+
+func (l *Libp2pHost) Close() error {
+	if err := l.host.Close(); err != nil {
+		return fmt.Errorf("failed to close host: %w", err)
+	}
+	log.Println("Host closed successfully")
+	return nil
+}
+
+func (l *Libp2pHost) GetHost() host.Host {
+	return l.host
+}
+
+func (l *Libp2pHost) GetPeerID() peer.ID {
+	return l.host.ID()
+}
+
+func (l *Libp2pHost) GetAddrs() []ma.Multiaddr {
+	addrs := make([]ma.Multiaddr, 0, len(l.host.Addrs()))
+	addrs = append(addrs, l.host.Addrs()...)
+	return addrs
+}
+
+func (l *Libp2pHost) GetPeerInfo() map[string][]string {
+	peerInfo := make(map[string][]string)
+	for _, peer := range l.host.Network().Peers() {
+		addresses := make([]string, 0, len(l.host.Peerstore().Addrs(peer)))
+		for _, addr := range l.host.Peerstore().Addrs(peer) {
+			addresses = append(addresses, addr.String())
+		}
+		peerInfo[peer.String()] = addresses
+	}
+	return peerInfo
+}
+
+func (l *Libp2pHost) Addrs() []ma.Multiaddr {
+	addrs := make([]ma.Multiaddr, 0, len(l.host.Addrs()))
+	addrs = append(addrs, l.host.Addrs()...)
+	return addrs
+}
+
+func (l *Libp2pHost) ID() string {
+	return l.host.ID().String()
+}
---
+++ sorting/process_manager.go
@@ -0,0 +1,89 @@
+package sorting
+
+import (
+	"fmt"
+	"log"
+	"sort"
+
+	"github.com/cpprian/distributed-sort-golang/messages"
+	"github.com/cpprian/distributed-sort-golang/serializers"
+
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+func (sm *SortingManager) ProcessMessage(msg messages.MessageInterface) {
+	switch msg.Type() {
+	case messages.CornerItemChange:
+		m := msg.(messages.CornerItemChangeMessage)
+		sm.ProcessCornerItemChange(m)
+	case messages.ItemExchange:
+		m := msg.(messages.ItemExchangeMessage)
+		if m.Response {
+			fmt.Println("Received ItemExchange response:", m)
+			sm.mu.Lock()
+			if m.SenderID > int64(sm.ID) {
+				sm.Items = append(sm.Items, m.OfferedItem)
+			} else {
+				sm.Items = append([]int64{m.OfferedItem}, sm.Items...)
+			}
+			intItems := make([]int, len(sm.Items))
+			for i, v := range sm.Items {
+				intItems[i] = int(v)
+			}
+			sort.Ints(intItems)
+			for i, v := range intItems {
+				sm.Items[i] = int64(v)
+			}
+			sm.mu.Unlock()
+		} else {
+			sm.RespondToItemsExchange(m)
+		}
+	case messages.AnnounceSelf:
+		m := msg.(messages.AnnounceSelfMessage)
+		sm.ParticipatingNodes[m.ID] = m.ListeningAddress.String()
+		sm.mu.Lock()
+		defer sm.mu.Unlock()
+		if sm.ID < m.ID {
+			sm.ID = m.ID
+		}
+		sm.Host.Broadcast(messages.NewAnnounceSelfMessage(int64(sm.ID), serializers.MultiaddrJSON{Multiaddr: ma.StringCast(sm.Host.GetListenAddress())}))
+		// Log the current participating nodes
+		log.Printf("Participating nodes updated: %v", sm.ParticipatingNodes)
+		// Notify that self has been announced
+		fmt.Println("AnnounceSelf received. Nodes now:", sm.ParticipatingNodes)
+	case messages.GetItems:
+		m := msg.(messages.GetItemsMessage)
+		sm.Host.SendMessage(messages.GetItemsMessage{Items: sm.Items})
+		fmt.Printf("GetItems request received from sender ID: %d. Current items: %v\n", m.SenderID, sm.Items)
+	case messages.ErrorType:
+		m := msg.(messages.ErrorMessage)
+		fmt.Println("Received error message:", m)
+	case messages.Confirm:
+		m := msg.(messages.ConfirmMessage)
+		fmt.Printf("Received confirmation for transaction ID: %s from sender ID: %d\n", m.TransactionID, m.SenderID)
+	case messages.NodesList:
+		m := msg.(messages.NodesListMessage)
+		fmt.Printf("Received NodesList from sender ID: %d with nodes: %v\n", m.SenderID, m.Nodes)
+		sm.mu.Lock()
+		defer sm.mu.Unlock()
+		for id, addr := range m.Nodes {
+			if _, exists := sm.ParticipatingNodes[int64(id)]; !exists {
+				sm.ParticipatingNodes[int64(id)] = addr
+			}
+		}
+		log.Printf("Updated participating nodes: %v", sm.ParticipatingNodes)
+	case messages.NodesListResponse:
+		m := msg.(messages.NodesListResponseMessage)
+		fmt.Printf("Received NodesListResponse from sender ID: %d with nodes: %v\n", m.SenderID, m.ParticipatingNodes)
+		sm.mu.Lock()
+		defer sm.mu.Unlock()
+		for id, addr := range m.ParticipatingNodes {
+			if _, exists := sm.ParticipatingNodes[id]; !exists {
+				sm.ParticipatingNodes[id] = addr.String()
+			}
+		}
+		log.Printf("Updated participating nodes from response: %v", sm.ParticipatingNodes)
+	default:
+		fmt.Println("Received unknown message:", msg)
+	}
+}
