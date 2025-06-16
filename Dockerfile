02ed18bf37a52fb5c63d3ff419b691b49c16936c
cpprian <cpprian456@gmail.com>
2025-06-16 18:13:57 +0200

    fix dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN go mod download
 
 COPY . .
 
-RUN CGO_ENABLED=0 GOOS=linux go build -o /bin/edcs-sort 
+RUN CGO_ENABLED=0 GOOS=linux go build -o /bin/edcs-sort ./bin
 
 FROM debian:bookworm-slim
 
--- networks/host.go
+++ networks/host.go
@@ -30,7 +30,7 @@ func NewEmptyLibp2pHost() *Libp2pHost {
 
 func NewLibp2pHost(messagingProtocol *MessagingProtocol) (*Libp2pHost, error) {
 	log.Println("Creating new Libp2pHost")
-	listenAddr, _ := ma.NewMultiaddr("/ip4/127.0.0.1/tcp/0")
+	listenAddr, _ := ma.NewMultiaddr("/ip4/0.0.0.0/tcp/0")
 
 	prvKey, _, err := crypto.GenerateEd25519Key(nil)
 	if err != nil {
